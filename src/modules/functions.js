911b3104c6e876de183b4184d2e617afcccfc1b1
TolikaTolika123 <tolikatolika794@gmail.com>
2022-01-20 14:25:46 +0400

    added todo adding functional

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,4 +1,3 @@
-import { add } from "date-fns";
 const projects = document.querySelector('.projects');
 
 const main = document.querySelector('.main')
@@ -17,32 +16,90 @@ class ProjectPrint {
   }
 
   html() {
-    let li = document.createElement('li');
+    const li = document.createElement('li');
     li.classList.add('projects__item');
-    let span = document.createElement('span')
+    const span = document.createElement('span')
     span.classList.add('projects__text')
     span.innerText = this.project.title;
     li.appendChild(span)
     li.addEventListener('click', () => {
-      main.innerHTML = `<div class="main__inner">
-    <h3 class="main__title">${this.project.title}</h3>
-    <ul class="main__list">
-      <li class="main__list-item">
-        <button class="main__add">
-          <span class="icon">
-            <svg width="13" height="13" fill="red"><path d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z"  fill-rule="evenodd"></path></svg>
-          </span>
-          Add task
-        </button>
-      </li>
-    </ul>
-  </div>`
+      addToMain(this.project)
     })
     projects.appendChild(li)
-
   }
 
+}
 
+function addToMain(project) {
+  main.innerHTML = '';
+  const mainInner = document.createElement('div');
+  mainInner.classList.add('main__inner');
+  const mainTitle = document.createElement('h3');
+  mainTitle.classList.add('main__title');
+  mainTitle.innerText = project.title;
+  const mainList = document.createElement('ul');
+  mainList.classList.add('main__list');
+
+  for (let i = 0; i < project.items.length; i++) {
+    const mainListItem = document.createElement('li');
+    mainListItem.classList.add('main__list-item');
+
+    const listItemTitle = document.createElement('p');
+    listItemTitle.innerText = project.items[i].title;
+    mainListItem.appendChild(listItemTitle)
+
+    const listItemDescription = document.createElement('p');
+    listItemDescription.innerText = project.items[i].description;
+    mainListItem.appendChild(listItemDescription)
+
+    mainList.appendChild(mainListItem)
+  }
+
+  mainList.innerHTML += `<li class="main__list-item">
+  <button class="main__add" data-project="${project.title}">
+    <span class="icon">
+      <svg width="13" height="13" fill="red"><path d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z"  fill-rule="evenodd"></path></svg>
+    </span>
+    Add task
+  </button>
+</li>`
+
+  mainInner.appendChild(mainTitle)
+  mainInner.appendChild(mainList)
+  main.appendChild(mainInner)
+
+  const addBtn = document.querySelector('.main__add');
+  addBtn.addEventListener('click', e => {
+    addNewItem(project, e)
+  })
+}
+
+function addNewItem(project, e) {
+  e.path[1].innerHTML = `<form class="main__item-create">
+  <div>
+    <input type="text" class="main__item-title" placeholder="e.g., Family lunch on Sunday at 11am">
+    <textarea class="main__item-description" placeholder="Description"></textarea>
+  </div>
+  <button class="main__item-add" disabled>Add tast</button>
+  <button class="main__item-cancel">Cancel</button>
+</form>`
+
+  const input = document.querySelector(".main__item-title");
+  const textarea = document.querySelector('.main__item-description')
+  const add = document.querySelector(".main__item-add");
+  input.addEventListener("input", () => {
+    input.value === "" ? (add.disabled = true) : (add.disabled = false);
+  });
+
+  add.addEventListener('click', () => {
+    project.items.push({
+      title: input.value,
+      description: textarea.value
+    })
+    console.log(project)
+    addToMain(project)
+  })
+  
 }
 
 addProjects.addEventListener('click', () => {
